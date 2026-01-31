00f953a204b810f6983d634307a14ff73a677a1f
nittjakul <nittjakul@gmail.com>
2026-02-01 00:14:23 +0700

    upload.js

--- api/upload.js
+++ api/upload.js
@@ -1,6 +1,17 @@
 import { put } from '@vercel/blob';
 
-export default async function handler(request, response) {
-  const blob = await put('folder/file.txt', request, { access: 'public' });
-  return response.status(200).json(blob);
+export default async function handler(request) {
+  const { searchParams } = new URL(request.url);
+  const filename = searchParams.get('filename');
+
+  // สั่งบันทึกไฟล์ลง Vercel Blob
+  const blob = await put(filename, request, {
+    access: 'public',
+  });
+
+  return Response.json(blob);
 }
+
+export const config = {
+  runtime: 'edge', // ใช้ Edge Runtime เพื่อความรวดเร็ว
+};
